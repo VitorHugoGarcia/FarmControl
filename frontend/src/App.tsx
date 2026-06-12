ab14912cb16676ad5d26156d10eb9c71cc4cf0c7
VitorHugoGarcia <vh.batistagarcia@gmail.com>
2026-06-11 23:29:56 -0400

    feat: adiciona navbar para navegação entre páginas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route> 
       </Routes>
     </HashRouter>
   );
