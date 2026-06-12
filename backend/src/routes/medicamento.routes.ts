3ec276f9234f6828371016e47727a4851abe5d40
VitorHugoGarcia <vh.batistagarcia@gmail.com>
2026-06-12 16:49:57 -0400

    fix: retira gerador de XML e PDF e lógica temporariamente.

--- backend/src/routes/medicamento.routes.ts
+++ backend/src/routes/medicamento.routes.ts
@@ -9,7 +9,6 @@ import {
 } from "../controllers/medicamento-CRUD.controller";
 import { upload } from "../config/multer";
 import { 
-  downloadArquivo, 
   realizarCompra 
 } from "../controllers/medicamento-Compra.controller";
 
@@ -23,6 +22,5 @@ router.post("/nota-fiscal", upload.single("xml"), processarNotaFiscal);
 router.put("/:id", atualizarMedicamento);
 router.delete("/:id", deletarMedicamento);
 router.post("/compra", realizarCompra);
-router.get("/compra/download/:arquivo", downloadArquivo);
 
 export default router;
